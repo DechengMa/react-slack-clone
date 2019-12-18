fe8416e58caa8fe79800694b7fe06fd928c9c631
Decheng <chinadecheng@gmail.com>
2019-12-18 18:58:45 +1100

    add emoji feature

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import firebase from '../../firebase';
 import uuidv4 from 'uuid/v4';
 import { Segment, Button, Input } from 'semantic-ui-react';
+import { Picker, emojiIndex } from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
 
 import FileModal from './FileModal';
 import ProgressBar from './ProgressBar';
@@ -9,6 +11,7 @@ import ProgressBar from './ProgressBar';
 class MessageForm extends Component {
 	state = {
 		storageRef: firebase.storage().ref(),
+		typingRef: firebase.database().ref('typing'),
 		uploadTask: null,
 		uploadState: '',
 		percentageUploaded: 0,
@@ -17,17 +20,64 @@ class MessageForm extends Component {
 		user: this.props.currentUser,
 		channel: this.props.currentChannel,
 		errors: [],
-		modal: false
+		modal: false,
+		emojiPicker: false
 	};
 
 	openModal = () => this.setState({ modal: true });
 
 	closeModal = () => this.setState({ modal: false });
 
+	handleTogglePicker = () => {
+		this.setState({ emojiPicker: !this.state.emojiPicker });
+	};
+
+	handleAddEmoji = emoji => {
+		const oldMessage = this.state.message;
+		const newMessage = this.colonToUnicode(` ${oldMessage} ${emoji.colons} `);
+		this.setState({ message: newMessage, emojiPicker: false });
+		setTimeout(() => this.messageInputRef.focus(), 0);
+	};
+
+	colonToUnicode = message => {
+		return message.replace(/:[A-Za-z0-9_+-]+:/g, x => {
+			x = x.replace(/:/g, '');
+			let emoji = emojiIndex.emojis[x];
+			if (typeof emoji !== 'undefined') {
+				let unicode = emoji.native;
+				if (typeof unicode !== 'undefined') {
+					return unicode;
+				}
+			}
+			x = ':' + x + ':';
+			return x;
+		});
+	};
+
 	handleChange = event => {
 		this.setState({
 			[event.target.name]: event.target.value
 		});
+	};
+
+	handleKeyDown = event => {
+		if (event.ctrlKey && event.keyCode === 13) {
+			this.sendMessage();
+		}
+
+		const { message, typingRef, channel, user } = this.state;
+
+		if (message) {
+			typingRef
+				.child(channel.id)
+				.child(user.uid)
+				.set(user.displayName);
+		} else {
+			typingRef
+				.child(channel.id)
+				.child(user.uid)
+				.remove();
+		}
 	};
 
 	createMassage = (fileURL = null) => {
@@ -50,14 +100,14 @@ class MessageForm extends Component {
 	};
 
 	sendMessage = () => {
-		const { messagesRef } = this.props;
-		const { message, channel } = this.state;
+		const { getMessagesRef } = this.props;
+		const { message, channel, user, typingRef } = this.state;
 
 		if (message) {
 			this.setState({
 				loading: true
 			});
-			messagesRef
+			getMessagesRef()
 				.child(channel.id)
 				.push()
 				.set(this.createMassage())
@@ -67,6 +117,11 @@ class MessageForm extends Component {
 						message: '',
 						errors: []
 					});
+
+					typingRef
+						.child(channel.id)
+						.child(user.uid)
+						.remove();
 				})
 				.catch(err => {
 					console.error(err);
@@ -84,10 +139,18 @@ class MessageForm extends Component {
 		}
 	};
 
+	getPath = () => {
+		if (this.props.isPrivateChannel) {
+			return `chat/private-${this.state.channel.id}`;
+		} else {
+			return 'chat/public';
+		}
+	};
+
 	uploadFile = (file, metadata) => {
 		const pathToUpload = this.state.channel.id;
-		const ref = this.props.messagesRef;
-		const filePath = `chat/public/${uuidv4().jpg}`;
+		const ref = this.props.getMessagesRef();
+		const filePath = `${this.getPath()}/${uuidv4()}.jpg`;
 
 		this.setState(
 			{
@@ -159,16 +222,33 @@ class MessageForm extends Component {
 			loading,
 			modal,
 			uploadState,
-			percentageUploaded
+			percentageUploaded,
+			emojiPicker
 		} = this.state;
 		return (
 			<Segment className='message__form'>
+				{emojiPicker && (
+					<Picker
+						set='apple'
+						onSelect={this.handleAddEmoji}
+						className='emojipicker'
+						title='Pick your emoji'
+						emoji='point_up'
+					/>
+				)}
 				<Input
 					fluid
 					name='message'
 					style={{ marginBottom: '0.7em' }}
 					value={message}
-					label={<Button icon={'add'} />}
+					label={
+						<Button
+							icon={emojiPicker ? 'close' : 'add'}
+							content={emojiPicker ? 'Close' : null}
+							onClick={this.handleTogglePicker}
+						/>
+					}
+					ref={node => (this.messageInputRef = node)}
 					labelPosition='left'
 					placeholder='Write your message'
 					className={
@@ -177,6 +257,7 @@ class MessageForm extends Component {
 							: ''
 					}
 					onChange={this.handleChange}
+					onKeyDown={this.handleKeyDown}
 				/>
 
 				<Button.Group icon widths='2'>
@@ -193,6 +274,7 @@ class MessageForm extends Component {
 						content='Upload Media'
 						labelPosition='right'
 						icon='cloud upload'
+						disabled={uploadState === 'uploading'}
 						onClick={this.openModal}
 					/>
 				</Button.Group>
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import { Segment, Comment } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { setUserPosts } from '../../actions';
 import firebase from '../../firebase';
 
 import MessageHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
 import Message from './Message';
+import Typing from './Typing';
 
 class Messages extends Component {
 	state = {
+		privateChannel: this.props.isPrivateChannel,
+		privateMessagesRef: firebase.database().ref('privateMessages'),
 		messagesRef: firebase.database().ref('messages'),
 		messages: [],
 		messagesLoading: true,
+		isChannelStarred: false,
 		channel: this.props.currentChannel,
-		user: this.props.currentUser
+		user: this.props.currentUser,
+		usersRef: firebase.database().ref('users'),
+		typingRef: firebase.database().ref('typing'),
+		numUniqueUsers: '',
+		searchTerm: '',
+		searchLoading: false,
+		searchResults: [],
+		typingUsers: [],
+		connectedRef: firebase.database().ref('.info/connected')
 	};
 
 	componentDidMount() {
@@ -20,17 +34,68 @@ class Messages extends Component {
 
 		if (channel && user) {
 			this.addListeners(channel.id);
+			this.addUserStarsListener(channel.id, user.uid);
 		}
 	}
 
 	addListeners = channelId => {
 		this.addMessageListener(channelId);
+		this.addTypingListeners(channelId);
+	};
+
+	addTypingListeners = channelId => {
+		let typingUsers = [];
+		this.state.typingRef.child(channelId).on('child_added', snap => {
+			if (snap.key !== this.state.user.uid) {
+				typingUsers = typingUsers.concat({
+					id: snap.key,
+					name: snap.val()
+				});
+				this.setState({ typingUsers });
+			}
+		});
+
+		this.state.typingRef.child(channelId).on('child_removed', snap => {
+			const index = typingUsers.findIndex(user => user.id === snap.key);
+			if (index !== -1) {
+				typingUsers = typingUsers.filter(user => user.id !== snap.key);
+				this.setState({ typingUsers });
+			}
+		});
+
+		this.state.connectedRef.on('value', snap => {
+			if (snap.val() === true) {
+				this.state.typingRef
+					.child(channelId)
+					.child(this.state.user.uid)
+					.onDisconnect()
+					.remove(err => {
+						if (err !== null) {
+							console.error(err);
+						}
+					});
+			}
+		});
+	};
+
+	addUserStarsListener = (channelId, userId) => {
+		this.state.usersRef
+			.child(userId)
+			.child('starred')
+			.once('value')
+			.then(data => {
+				if (data.val() !== null) {
+					const channelIds = Object.keys(data.val());
+					const prevStarred = channelIds.includes(channelId);
+					this.setState({ isChannelStarred: prevStarred });
+				}
+			});
 	};
 
 	addMessageListener = channelId => {
 		let loadedMessages = [];
-
-		this.state.messagesRef.child(channelId).on('child_added', snap => {
+		const ref = this.getMessagesRef();
+		ref.child(channelId).on('child_added', snap => {
 			loadedMessages.push(snap.val());
 
 			this.setState({
@@ -38,6 +103,73 @@ class Messages extends Component {
 				messagesLoading: false
 			});
 		});
+		this.countUniqueUsers(loadedMessages);
+		this.countUserPosts(loadedMessages);
+	};
+
+	countUserPosts = messages => {
+		let userPosts = messages.reduce((acc, message) => {
+			if (message.user.name in acc) {
+				acc[message.user.name].count += 1;
+			} else {
+				acc[message.user.name] = {
+					avatar: message.user.avatar,
+					count: 1
+				};
+			}
+			return acc;
+		}, {});
+		this.props.setUserPosts(userPosts);
+	};
+
+	handleSearchChange = event => {
+		this.setState(
+			{
+				searchTerm: event.target.value,
+				searchLoading: true
+			},
+			() => this.handleSearchMessages()
+		);
+	};
+
+	handleSearchMessages = () => {
+		const changeMessages = [...this.state.messages];
+		const regex = new RegExp(this.state.searchTerm, 'gi');
+		const searchResults = changeMessages.reduce((acc, message) => {
+			if (
+				(message.content && message.content.match(regex)) ||
+				message.user.name.match(regex)
+			) {
+				acc.push(message);
+			}
+
+			return acc;
+		}, []);
+		this.setState({ searchResults });
+		setTimeout(() => {
+			this.setState({ searchLoading: false });
+		}, 1000);
+	};
+
+	countUniqueUsers = messages => {
+		const uniqueUsers = messages.reduce((acc, message) => {
+			if (!acc.includes(message.user.name)) {
+				acc.push(message.user.name);
+			}
+			return acc;
+		}, []);
+
+		const plural = uniqueUsers.length > 1 || uniqueUsers.length === 0;
+		const numUniqueUsers = `${uniqueUsers.length} User${plural ? 's' : ''}`;
+
+		this.setState({
+			numUniqueUsers
+		});
+	};
+
+	getMessagesRef = () => {
+		const { messagesRef, privateMessagesRef, privateChannel } = this.state;
+		return privateChannel ? privateMessagesRef : messagesRef;
 	};
 
 	displayMessages = messages =>
@@ -50,15 +182,91 @@ class Messages extends Component {
 			/>
 		));
 
+	handleStar = () => {
+		this.setState(
+			prevState => ({
+				isChannelStarred: !prevState.isChannelStarred
+			}),
+			() => this.starChannel()
+		);
+	};
+
+	starChannel = () => {
+		if (this.state.isChannelStarred) {
+			this.state.usersRef.child(`${this.state.user.uid}/starred`).update({
+				[this.state.channel.id]: {
+					name: this.state.channel.name,
+					details: this.state.channel.details,
+					createdBy: {
+						name: this.state.channel.createdBy.name,
+						avatar: this.state.channel.createdBy.avatar
+					}
+				}
+			});
+		} else {
+			this.state.usersRef
+				.child(`${this.state.user.uid}/starred`)
+				.child(this.state.channel.id)
+				.remove(err => {
+					if (err !== null) {
+						console.error(err);
+					}
+				});
+		}
+	};
+
+	displayChannelName = channel => {
+		return channel
+			? `${this.state.privateChannel ? '@' : '#'}${channel.name}`
+			: '';
+	};
+
+	displayTypingUsers = users =>
+		users.length > 0 &&
+		users.map(user => (
+			<div
+				style={{ display: 'flex', alignItems: 'center', marginBottom: '0.2em' }}
+				key={user.id}
+			>
+				<span className='user__typing'>{user.name} is typing</span> <Typing />
+			</div>
+		));
+
 	render() {
-		const { messagesRef, messages, channel, user } = this.state;
+		const {
+			messagesRef,
+			messages,
+			channel,
+			user,
+			numUniqueUsers,
+			searchTerm,
+			searchResults,
+			searchLoading,
+			privateChannel,
+			isChannelStarred,
+			typingUsers,
+			messagesLoading
+		} = this.state;
 		return (
 			<>
-				<MessageHeader />
+				<MessageHeader
+					channelName={this.displayChannelName(channel)}
+					numUniqueUsers={numUniqueUsers}
+					handleSearchChange={this.handleSearchChange}
+					searchLoading={searchLoading}
+					isPrivateChannel={privateChannel}
+					handleStar={this.handleStar}
+					isChannelStarred={isChannelStarred}
+				/>
 
 				<Segment>
-					<Comment.Group style={{ maxWidth: '100vw' }} className='messages'>
-						{this.displayMessages(messages)}
+					<Comment.Group className='messages'>
+						{this.displayMessageSkeleton(messagesLoading)}
+						{searchTerm
+							? this.displayMessages(searchResults)
+							: this.displayMessages(messages)}
+						{this.displayTypingUsers(typingUsers)}
+						<div ref={node => (this.messagesEnd = node)} />
 					</Comment.Group>
 				</Segment>
 
@@ -66,10 +274,12 @@ class Messages extends Component {
 					messagesRef={messagesRef}
 					currentChannel={channel}
 					currentUser={user}
+					isPrivateChannel={privateChannel}
+					getMessagesRef={this.getMessagesRef}
 				/>
 			</>
 		);
 	}
 }
 
-export default Messages;
+export default connect(null, { setUserPosts })(Messages);
